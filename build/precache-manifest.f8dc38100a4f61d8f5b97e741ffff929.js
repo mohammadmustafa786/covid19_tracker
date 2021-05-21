67a8e1da21eb0d50dfb4c0d1ef230fafda23c86a
Muhammad Mustafa <41040538+mohammadmustafa786@users.noreply.github.com>
2021-05-21 19:24:47 +0500

    Add files via upload

---
+++ build/precache-manifest.f8dc38100a4f61d8f5b97e741ffff929.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55240726a7aaa2fdf1a85722caef168d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01e66130ecea0eb83ca4",
+    "url": "/static/css/main.09897bb7.chunk.css"
+  },
+  {
+    "revision": "b6742956d0d73730b709",
+    "url": "/static/js/2.ea59d149.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.ea59d149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01e66130ecea0eb83ca4",
+    "url": "/static/js/main.3927621e.chunk.js"
+  },
+  {
+    "revision": "e173dfe45a061c6e2715",
+    "url": "/static/js/runtime-main.20a4ba51.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
